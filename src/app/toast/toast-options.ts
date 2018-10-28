903a29173bc6555a85995b20ef9aa3a07cd58a9b
Kevin Lin <kevin.lin@zuhlke.com>
2018-10-28 20:30:02 +0800

    Adjust default options

--- src/app/toast/toast-options.ts
+++ src/app/toast/toast-options.ts
@@ -3,16 +3,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ToastOptions {
   positionClass = 'toast-top-right';
-  maxShown = 5;
+  maxShown = 10;
   newestOnTop = true;
   animate = 'fade';
 
   // override-able properties
+  dismiss = 'click'; // 'auto' | 'click' | 'controlled'
   toastLife = 5000;
   enableHTML = false;
-  dismiss = 'click'; // 'auto' | 'click' | 'controlled'
-  messageClass = 'toast-message';
   titleClass = 'toast-title';
+  messageClass = 'toast-message';
   showCloseButton = false;
 
   constructor() {}
